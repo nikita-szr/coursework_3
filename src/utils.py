d00a21dd1ac53f0c9fb5a94b322e55b63cece696
nikitt <nikittaosipov@yandex.ru>
2024-08-21 21:37:15 +0400

    функции для заполнения таблиц

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 
-def create_database(database_name: str, params: dict):
+def create_database(database_name, params):
     """Функция создаёт базу данных и таблицы"""
 
     conn = None  # без этой строки ошибка, надо спросить почему
@@ -46,3 +46,51 @@ def create_database(database_name: str, params: dict):
     finally:
         if conn:
             conn.close()
+
+
+def save_data_to_database_emp(data_emp, database_name, params) -> None:
+    """Функция для заполнения таблицы работодателей в БД"""
+    conn = psycopg2.connect(dbname=database_name, **params)
+
+    try:
+        with conn.cursor() as cur:
+            for emp in data_emp:
+                cur.execute("""
+                    INSERT INTO employers (employer_id, employer_name, employer_area, url, open_vacancies)
+                    VALUES (%s, %s, %s, %s, %s)
+                    ON CONFLICT (employer_id) DO NOTHING
+                    """, (emp['id'], emp['name'], emp['area']['name'], emp['alternate_url'],
+                          emp['open_vacancies']))
+
+        conn.commit()
+
+    except Exception as e:
+        print(f"Ошибка при сохранении данных работодателей: {e}")
+
+    finally:
+        conn.close()
+
+
+def save_data_to_database_vac(data_vac, database_name, params) -> None:
+    """Функция для заполнения таблицы вакансий в БД"""
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+
+    try:
+        with conn.cursor() as cur:
+            for vac in data_vac:
+                salary = vac['salary']['from'] if vac['salary'] and vac['salary']['from'] is not None else None
+                cur.execute("""
+                    INSERT INTO vacancy (vacancy_id, vacancy_name, vacancy_area, salary, employer_id, vacancy_url)
+                    VALUES (%s, %s, %s, %s, %s, %s)
+                    ON CONFLICT (vacancy_id) DO NOTHING
+                    """, (vac['id'], vac['name'], vac['area']['name'], salary, vac['employer']['id'],
+                          vac['alternate_url']))
+
+        conn.commit()
+
+    except Exception as e:
+        print(f"Ошибка при сохранении данных вакансий: {e}")
+
+    finally:
+        conn.close()
